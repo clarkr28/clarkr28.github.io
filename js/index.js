34894d33a4a34f07b3613d9429789a322152ffde
Jared Clark <rjaredclark@gmail.com>
2022-11-28 22:15:40 -0600

    redirecting to github 404 page

---
+++ js/index.js
@@ -0,0 +1,4 @@
+/* this website is so out of date that I don't really want people looking 
+ * at it, so I'm redirecting to a github 404 page :)
+ */
+window.location.replace("https://github.com/nonexistentpageurl"); 
